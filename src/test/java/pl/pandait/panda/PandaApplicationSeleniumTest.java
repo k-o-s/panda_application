8d21cec9c09e4f11fe9177587e32b94dd3e79bea
ksokala <sokala9@gmail.com>
2020-09-16 17:53:05 +0000

    Change in test

--- src/test/java/pl/pandait/panda/PandaApplicationSeleniumTest.java
+++ src/test/java/pl/pandait/panda/PandaApplicationSeleniumTest.java
@@ -18,7 +18,7 @@ import org.springframework.boot.web.server.LocalServerPort;
 import static org.junit.Assert.assertEquals;
 
 
-@SpringBootTest(classes = {PandaApplication.class}, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+// @SpringBootTest(classes = {PandaApplication.class}, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class PandaApplicationSeleniumTest {
 
     private static WebDriver driver;
@@ -40,7 +40,7 @@ public class PandaApplicationSeleniumTest {
         driver = new RemoteWebDriver(new URL("http://192.168.44.44:4444/wd/hub"), capabilities);
 
         // Pamiętaj, że aplikacja Spring musi działać! To znaczy też musi być włączona.
-        driver.get(String.format("http://ubuntu:%d/", port));
+        driver.get(String.format("http://192:168:44:44:%d/", 8080));
 
         //Czekamy 2 sekundy
         Thread.sleep(2000);
